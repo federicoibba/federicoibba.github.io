9f8d0e4493d2ee0e41c2d903b66144cbb1ffc20e
Federico Ibba <federico.ibba9@gmail.com>
2025-10-25 12:28:03 +0200

    fix: style in project views less than lg breakpoint

--- app/pages/projects/[project].vue
+++ app/pages/projects/[project].vue
@@ -9,16 +9,16 @@
       </template>
 
       <!-- Header -->
-      <div class="flex flex-row flex-wrap lg:flex-nowrap gap-8 py-8">
+      <div class="flex flex-col md:flex-row flex-wrap md:flex-nowrap items-center gap-8 py-8">
         <NuxtImg
           :src="project.image"
           class="rounded-lg"
-          fit="contain"
-          sizes="100vw sm:50vw md:800px"
+          fit="cover"
+          sizes="100vw md:800px"
           style="max-width: 400px; max-height: 300px;"
         />
-        <div class="flex flex-col h-full items-center lg:items-start text-center lg:text-left gap-2 lg:gap-4">
-          <h1 class="text-2xl md:text-4xl font-bold ">
+        <div class="flex flex-col h-full items-center md:items-start text-center md:text-left gap-2 lg:gap-4">
+          <h1 class="text-2xl md:text-4xl font-bold">
             {{ project.title }}
           </h1>
           <p>
@@ -34,11 +34,11 @@
           >
             Github
           </UButton>
-          <div class="hidden lg:flex flex-col">
+          <div class="hidden md:flex flex-col">
             <h2 class="text-xl md:text-2xl font-semibold mb-2">
               {{ $t('projects.technologies') }}
             </h2>
-            <div class="flex flex-wrap w-full justify-center lg:justify-start gap-4 mt-auto">
+            <div class="flex flex-wrap w-full justify-center md:justify-start gap-4 mt-auto">
               <UBadge
                 v-for="tech in project.technologies"
                 :key="tech"
