39b712db1a675ebbc9b0947b35d4cc1199ff14b2
Federico Ibba <federico.ibba9@gmail.com>
2025-10-25 12:33:47 +0200

    refactor: create header component for project

---
+++ app/components/Project/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex flex-row flex-wrap lg:flex-nowrap gap-8 p-8 justify-center">
+    <NuxtImg
+      :src="project.image"
+      class="rounded-lg"
+      fit="contain"
+      sizes="100vw sm:50vw md:800px"
+      style="max-width: 400px; max-height: 300px;"
+    />
+    <div class="flex flex-col h-full items-center lg:items-start text-center lg:text-left gap-2 lg:gap-4">
+      <h1 class="text-2xl md:text-4xl font-bold">
+        {{ project.title }}
+      </h1>
+      <p>
+        {{ project.description }}
+      </p>
+      <UButton
+        icon="i-simple-icons-github"
+        :to="project.github"
+        target="_blank"
+        class="w-fit"
+        size="lg"
+        color="primary"
+      >
+        Github
+      </UButton>
+      <div class="hidden lg:flex flex-col">
+        <h2 class="text-xl md:text-2xl font-semibold mb-2">
+          {{ $t('projects.technologies') }}
+        </h2>
+        <div class="flex flex-wrap w-full justify-center lg:justify-start gap-4 mt-auto">
+          <UBadge
+            v-for="tech in project.technologies"
+            :key="tech"
+            size="xl"
+            variant="outline"
+          >
+            {{ tech }}
+          </UBadge>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { ProjectsCollectionItem } from '@nuxt/content'
+
+interface ProjectHeaderProps {
+  project: ProjectsCollectionItem
+}
+
+defineProps<ProjectHeaderProps>()
+</script>
--- app/pages/projects/[project].vue
+++ app/pages/projects/[project].vue
@@ -9,48 +9,7 @@
       </template>
 
       <!-- Header -->
-      <div class="flex flex-col md:flex-row flex-wrap md:flex-nowrap items-center gap-8 py-8">
-        <NuxtImg
-          :src="project.image"
-          class="rounded-lg"
-          fit="cover"
-          sizes="100vw md:800px"
-          style="max-width: 400px; max-height: 300px;"
-        />
-        <div class="flex flex-col h-full items-center md:items-start text-center md:text-left gap-2 lg:gap-4">
-          <h1 class="text-2xl md:text-4xl font-bold">
-            {{ project.title }}
-          </h1>
-          <p>
-            {{ project.description }}
-          </p>
-          <UButton
-            icon="i-simple-icons-github"
-            :to="project.github"
-            target="_blank"
-            class="w-fit"
-            size="lg"
-            color="primary"
-          >
-            Github
-          </UButton>
-          <div class="hidden md:flex flex-col">
-            <h2 class="text-xl md:text-2xl font-semibold mb-2">
-              {{ $t('projects.technologies') }}
-            </h2>
-            <div class="flex flex-wrap w-full justify-center md:justify-start gap-4 mt-auto">
-              <UBadge
-                v-for="tech in project.technologies"
-                :key="tech"
-                size="xl"
-                variant="outline"
-              >
-                {{ tech }}
-              </UBadge>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ProjectHeader :project="project" />
 
       <!-- Content -->
       <ContentRenderer
